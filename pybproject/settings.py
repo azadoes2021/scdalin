4f9df27e752f4e4764bb635a97dd9400b53dbe75
azafree <azafree@daum.net>
2025-07-02 13:42:46 +0900

    first commit

--- pybproject/settings.py
+++ pybproject/settings.py
@@ -44,8 +44,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 DEBUG = True 
 
 
-# ALLOWED_HOSTS =['3.36.9.42', 'sotoplus.co.kr']
-ALLOWED_HOSTS =['3.35.38.28']
+ALLOWED_HOSTS =['3.35.38.28', 'pagefyt.com']
 
 
 # Application definition
