64a5a99ae86c60acd5859347962e8765849cfd3f
azafree <azafree@daum.net>
2025-07-10 10:52:27 +0900

    dbcommit

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 from . import views
-from .views import HomeView, CollectdbView, CollectdbView002
+from .views import HomeView, CollectdbView, CollectdbView002, CollectdbView003
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
@@ -10,6 +10,7 @@ urlpatterns = [
 
     path('collectingdb', CollectdbView.as_view(), name="collectingdb"),
     path('collectingdb002', CollectdbView002.as_view(), name="collectingdb002"),
+    path('collectingdb003', CollectdbView003.as_view(), name="collectingdb003"),
     path('successori2',views.successori2 , name="successori2"),
     path('successree2',views.successree2 , name="successree2"),    
 
